c4fc39855891cbbe52787fafa8d3daf199da3c24
raybagas7 <raysam.gas@gmail.com>
2024-04-12 04:34:11 +0700

    faet(PostCreate): - create a new route for new post

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,27 @@ import { RouterLink, RouterView } from 'vue-router'
 <template>
   <header>
     <nav>
-      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/" class="nav-link">Home</RouterLink>
+      <RouterLink to="/post-create" class="nav-link">New Post</RouterLink>
     </nav>
   </header>
-
   <RouterView />
 </template>
+
+<style lang="scss" scoped>
+nav {
+  padding: 1rem 2rem;
+  background: #1e40af;
+  border-bottom: 1px solid #60a5fa;
+  .nav-link {
+    color: #fff;
+    padding: 5px 15px;
+    border-radius: 5px;
+    transition: all 0.2s ease-in-out;
+    margin-right: 1rem;
+    &:hover {
+      background: #60a5fa;
+    }
+  }
+}
+</style>
