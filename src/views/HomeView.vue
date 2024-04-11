db41a740c807fe78157710648a245aefc7b926a6
raybagas7 <raysam.gas@gmail.com>
2024-04-11 10:55:30 +0700

    feat(slot): - add slot tag to the wrapper

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <script setup>
 import PostItem from '@/components/PostItem.vue'
-
+import MyWrapper from '@/components/MyWrapper.vue'
 const posts = [
   {
     id: 1,
@@ -39,21 +39,8 @@ const posts = [
 
 <template>
   <div v-for="post in posts" :key="post.id">
-    <div class="wrapper">
+    <MyWrapper>
       <PostItem :post="post" />
-    </div>
+    </MyWrapper>
   </div>
 </template>
-
-<style lang="scss" scoped>
-.wrapper {
-  margin: 2rem;
-  background: #fff;
-  box-shadow:
-    rgba(0, 0, 0, 0.1) 0px 10px 15px -3px,
-    rgba(0, 0, 0, 0.05) 0px 4px 6px -2px;
-  margin-bottom: 2rem;
-  border-radius: 1rem;
-  overflow: hidden;
-}
-</style>
